826e250c9c808e4ac16ff4f8edb515e6a038bfe9
Tasselkk <326468090@qq.com>
2020-02-14 18:14:24 +0800

    Maven项目测试

---
+++ src/main/java/demo/simple.java
@@ -0,0 +1,12 @@
+package demo;
+import org.apache.commons.math3.util.ArithmeticUtils;
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int a =ArithmeticUtils.gcd(365, 285);
+		System.out.println(a);
+
+	}
+
+}
